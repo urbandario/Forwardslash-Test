ea2c08696e42bcf541839ebf94db093f320dd1a1
Dario Urban <urbandario98@gmail.com>
2023-06-24 17:14:26 +0200

    Task 1 Update: Added try catch

--- task1/calculator.php
+++ task1/calculator.php
@@ -1,9 +1,23 @@
 <?php
 class Calculator {
     public static function addNumbers(...$numbers) {
+        if (empty($numbers)) {
+            throw new Exception('No numbers provided.');
+        }
+        
+        foreach ($numbers as $number) {
+            if (!is_numeric($number)) {
+                throw new Exception('Invalid input. Non-numeric value detected.');
+            }
+        }
+
         return array_sum($numbers);
     }
 }
 
-$result1 = Calculator::addNumbers(3, 5, 6, 58, 99);
-echo $result1;
+try {
+    $result1 = Calculator::addNumbers(5, 6, 6);
+    echo $result1;
+} catch (Exception $e) {
+    echo 'Exception: ' . $e->getMessage();
+}
